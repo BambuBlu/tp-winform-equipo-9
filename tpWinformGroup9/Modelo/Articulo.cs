86a3c51fd6de847f8650b2e2203d97d0ea024be8
BambuBlu <55064080+BambuBlu@users.noreply.github.com>
2024-04-22 19:17:28 -0300

    Added new classes and mods to the class item.

--- tpWinformGroup9/Modelo/Articulo.cs
+++ tpWinformGroup9/Modelo/Articulo.cs
@@ -1,37 +1,20 @@
-﻿using System.Security.Principal;
+﻿using System.ComponentModel;
+using System;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
 
 namespace tpWinformGroup9.Modelo
 {
     public class Articulo
     {
-        private string nombre;
-        private string marca;
-        private float precio;
-        private int cantidad;
-
-        public string Nombre
-        { get { return nombre; }
-
-        set { nombre = value; }
-        }
-        public string Marca
-        {
-            get { return marca; }
-
-            set { marca = value; }
-        }
-        public float Precio
-        {
-            get { return precio; }
-
-            set { precio = value; }
-        }
-        public int Cantidad
-        {
-            get { return cantidad; }
-
-            set { cantidad = value; }
-        }
+        public int ID { get; set; }
+        public String Codigo { get; set; }
+        public String Nombre { get; set; }
+        public String Descripcion { get; set; }
+        public Marca Marca { get; set; }  //contendria la id, la descripcion y alguna posible funcion
+        public Categoria Categoria { get; set; } //contendria la id, la descripcion y alguna posible funcion
+        public decimal Precio { get; set; }
+        public Imagen Imagen { get; set; } //contendria la conexion de id con id de articulo, la url y alguna posible funcion
 
 
 
---
+++ tpWinformGroup9/Modelo/Imagen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Habria que marcar los parametros
+
+namespace tpWinformGroup9.Modelo
+{
+    internal class Imagen
+    {
+    }
+}
