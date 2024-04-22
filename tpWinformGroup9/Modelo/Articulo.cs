bf94d35616f8c963e5300abeb6b71ebc0d396fbb
Maximiliano Rey <maximilianogrey@hotmail.com>
2024-04-21 22:44:17 -0300

    agrego clase articulo datos modifique articulos y agregue un scrip de base de datos

--- tpWinformGroup9/Modelo/Articulo.cs
+++ tpWinformGroup9/Modelo/Articulo.cs
@@ -1,7 +1,39 @@
-﻿namespace tpWinformGroup9.Modelo
+﻿using System.Security.Principal;
+
+namespace tpWinformGroup9.Modelo
 {
     public class Articulo
     {
+        private string nombre;
+        private string marca;
+        private float precio;
+        private int cantidad;
+
+        public string Nombre
+        { get { return nombre; }
+
+        set { nombre = value; }
+        }
+        public string Marca
+        {
+            get { return marca; }
+
+            set { marca = value; }
+        }
+        public float Precio
+        {
+            get { return precio; }
+
+            set { precio = value; }
+        }
+        public int Cantidad
+        {
+            get { return cantidad; }
+
+            set { cantidad = value; }
+        }
+
+
 
     }
 }
---
+++ tpWinformGroup9/Negocio/ArticuloDatos.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tpWinformGroup9.Modelo;
+using System.Data.SqlClient; //Se debe agregar esta libreria para trabajar con base de datos
+
+namespace tpWinformGroup9.Negocio
+{
+     class ArticuloDatos
+    {
+        public List<Articulo> listar()
+        { 
+          List<Articulo> lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();//se iniciliza la conexion 
+            SqlCommand comando = new SqlCommand();//se inicializa el comando
+            SqlDataReader lector;//se crea el obejeto lector 
+
+            try
+            {
+                /*Se establecen los parametros de conexion en la seccion de server esta el . para no tener que cambiar nada 
+                 * indistintamente para cual de nosotros este haciendo pruebas con el codigo, la otra opccion es poner el nombre del desktop segun figura en tu pc */
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=TP_2_PROGRAMACION_3; integrated security=true"; /* en la seccion de integrated security si estas usando la autentificaciond de sql server tenes que poner el usuario y la contraseña  */
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "SELECT NOMBRE, MARCA, PRECIO, CANTIDAD FROM ARTICULO";
+                comando.Connection = conexion;
+                
+                conexion.Open();
+                lector = comando.ExecuteReader();
+                while (lector.Read())
+                {
+                    Articulo aux = new Articulo();
+
+                    aux.Nombre = (string)lector["NOMBRE"];
+                    aux.Marca = (string)lector["MARCA"];
+                    aux.Precio = (float)lector.GetDecimal(2); //validar el tipo de dato que sera traido de la DB y entre () el subindice de la columna
+                    aux.Cantidad = lector.GetInt32(3);
+
+                    lista.Add(aux);
+                }
+
+                conexion.Close();
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+           
+
+        }
+    }
+}
