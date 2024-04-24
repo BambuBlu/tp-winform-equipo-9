a2a649bfa1c88e843c66f9778f586c35817695f9
Maximiliano Rey <maximilianogrey@hotmail.com>
2024-04-23 23:12:21 -0300

    Agrego fincionalidad en combobox con seleccion de articulos que que genera seleccion de datos que aparecen en la gridbox

--- tpWinformGroup9/Modelo/Articulo.cs
+++ tpWinformGroup9/Modelo/Articulo.cs
@@ -8,13 +8,14 @@ namespace tpWinformGroup9.Modelo
     public class Articulo
     {
         public int ID { get; set; }
+        public int Cantidad { get; set; }
         public String Codigo { get; set; }
         public String Nombre { get; set; }
         public String Descripcion { get; set; }
-        public Marca Marca { get; set; }  //contendria la id, la descripcion y alguna posible funcion
-        public Categoria Categoria { get; set; } //contendria la id, la descripcion y alguna posible funcion
+        public String Marca { get; set; }  //contendria la id, la descripcion y alguna posible funcion
+        public String Categoria { get; set; } //contendria la id, la descripcion y alguna posible funcion
         public decimal Precio { get; set; }
-        public Imagen Imagen { get; set; } //contendria la conexion de id con id de articulo, la url y alguna posible funcion
+        public string Imagen { get; set; } //contendria la conexion de id con id de articulo, la url y alguna posible funcion
 
 
 
--- tpWinformGroup9/Principal.cs
+++ tpWinformGroup9/Principal.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
+using tpWinformGroup9.Modelo;
+using tpWinformGroup9.Negocio;
 
 namespace tpWinformGroup9
 {
     public partial class frmPrincipal : Form
     {
+        
         public frmPrincipal()
         {
             InitializeComponent();
@@ -20,6 +24,60 @@ namespace tpWinformGroup9
         {
             frmAddModify addWindow = new frmAddModify();
             addWindow.ShowDialog();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+          
+        
+            AccesoDatos accesoDatos = new AccesoDatos();
+            accesoDatos.setConsulta("SELECT NOMBRE FROM ARTICULOS");
+            accesoDatos.ejecutarLectura();
+            
+            while (accesoDatos.Lector.Read())
+            {
+                Articulo aux = new Articulo();
+                aux.Nombre = (string)accesoDatos.Lector["NOMBRE"];
+                listaArticulos.Items.Add(aux.Nombre);
+
+            }
+
+            accesoDatos.cerrarConexion();
+        }
+
+        private void listaArticulos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<Articulo> lart = new List<Articulo>();
+            AccesoDatos accesoDatos = new AccesoDatos();
+            accesoDatos.setConsulta("SELECT NOMBRE, MARCA, PRECIO, CANTIDAD, URLIMAGEN FROM ARTICULOS");
+            accesoDatos.ejecutarLectura();
+            while (accesoDatos.Lector.Read())
+            {
+                Articulo aux = new Articulo();
+                aux.Nombre = (string)accesoDatos.Lector["NOMBRE"];
+                aux.Marca = (string)accesoDatos.Lector["MARCA"];
+                aux.Precio = accesoDatos.Lector.GetDecimal(2);
+                aux.Cantidad = accesoDatos.Lector.GetInt32(3);
+
+                lart.Add(aux);
+                
+
+            }
+            string seleccion = listaArticulos.SelectedItem.ToString();
+            List<Articulo> articuloSeleccionado = new List<Articulo>();
+            for (int i = 0; i < lart.Count; i++)
+            {
+
+                if (seleccion == lart[i].Nombre)
+                { 
+                    articuloSeleccionado.Add(lart[i]);
+                
+                }
+               
+            }
+            dgvArticulos.DataSource = articuloSeleccionado;
+            accesoDatos.cerrarConexion();
+
         }
     }
 }
